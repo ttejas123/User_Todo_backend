ae4ef653a8b5a516f0215c20bd16e48107b5caad
ttejas123 <tthakare73@gmail.com>
2021-04-14 15:33:30 +0530

    online

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');//cross origin resource sharing (share api data with react app)
+//const bodyParser = require('body-parser');
 const taskTodo = require('./routes/taskTodo');
 const userProfile = require('./routes/userProfile');
 const project = require('./routes/project');
@@ -9,6 +10,9 @@ const project = require('./routes/project');
 
 //const team = require('./routes/team');
 const port = process.env.PORT || 3001;
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+app.use(cors());
 
 app.use(express.json());
 const monoDataBase_url = "mongodb+srv://admin:xZnRY7knPOQ0Bdr0@usercurd.teudj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" 
